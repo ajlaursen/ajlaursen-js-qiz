7692c4179fc6462db4aa0db28f50a4aa00d390f7
Alex Laursen <ajlaursen@aol.com>
2020-10-05 17:36:44 -0400

    started JS file

---
+++ quiz.js
@@ -0,0 +1,8 @@
+var question = document.getElementById("question");
+var answer1 = document.getElementById("answer-1");
+var answer2 = document.getElementById("answer-2");
+var answer3 = document.getElementById("answer-3");
+var answer4 = document.getElementById("answer-4");
+var score = document.getElementById("score");
+
+
