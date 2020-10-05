a4056cbd6cdd12d0ba3f33add236b8ac4a2dde52
Alex Laursen <ajlaursen@aol.com>
2020-10-05 19:33:30 -0400

    psuedo coding

--- quiz.js
+++ quiz.js
@@ -6,3 +6,11 @@ var answer4 = document.getElementById("answer-4");
 var score = document.getElementById("score");
 
 
+// create content for questions and answers
+// create functions to popluate questions and answers
+// creat functions/event listeners to watch for answer clicks
+// create functions to manipulate BG of cards to show correct or incorrect answer
+// create function to update score display
+// create button to move to next question and reset car BG to default
+// once all questions are answered create score list that shows user scores from all previous attempts
+// create timer that starts on start click
